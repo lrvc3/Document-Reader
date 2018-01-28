e24297827dd122db8a34a08671c3c2e9dacbfe98
Renita Lobo <rennylobo3@gmail.com>
2018-01-28 10:48:55 +0530

    Document Reader- Initial Version 0

---
+++ doc_reader_0.py
@@ -0,0 +1,13 @@
+import pyttsx3
+
+engine = pyttsx3.init()
+##
+##engine.setProperty('rate',120)  #120 words per minute
+##engine.setProperty('volume',0.9) 
+
+with open("readfile.txt") as file:
+    for line in file:
+        engine.say(line)
+        engine.runAndWait()
+ 
+        
